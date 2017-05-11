493d25d3d93a66739088321e97818078a81935a1
pilium <igorpilium@gmail.com>
2017-05-11 17:27:10 +0300

    Верстка секций Стоимость , наша команда , партнер

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,4 +1,4 @@
-;
+
 // Начинать писать отсюда!!!!
 $(document).ready(function(){
     $('.header-nav').click(function(){
@@ -9,5 +9,25 @@ $(document).ready(function(){
         } else {
             menu.slideDown();
         }
-    })
+    });
+    $('.js-reviews-slider').slick({
+        dots: true,
+        speed: 300,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        prevArrow: '.reviews-slider__prev',
+        nextArrow: '.reviews-slider__next',
+        responsive: [
+    {
+        breakpoint: 1024,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        }
+  ]
+
+    });
 });
